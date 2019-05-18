e814f155a3d009563a8b9a40f5d0dfc8142f8d81
sailar305 <sailar305@hotmail.com>
2019-05-18 13:17:30 -0500

    Update paciente.php

--- paciente.php
+++ paciente.php
@@ -28,7 +28,11 @@
     			<h2 class="py-2 px-2 d-none d-md-inline-block" href="#modal-select-paciente" data-toggle="modal" >Paciente</h2>
           <button type="button" class="btn btn-info" style="max-width: 40%; max-height:  50%;" data-dismiss="modal">info</button>
     		</div>
-    		<div class="col-5 container d-flex flex-column flex-md-row">
+    		<div class="col-6 container d-flex flex-column flex-md-row">
+          <div class="col-4 container d-flex flex-column flex-md-row"></div>
+          <div class="col-2 container d-flex flex-column flex-md-row">
+            <a class="text-dark" href="">cerrar session</a>
+            </div>
     		</div>
     	</div>
   </nav>
