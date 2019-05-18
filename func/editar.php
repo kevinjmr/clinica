0974263f03d7bfb6d39ca22d53d68b32e5d9e768
kevinjmr <44417575+kevinjmr@users.noreply.github.com>
2019-05-18 15:27:50 -0500

    Update editar.php

--- func/editar.php
+++ func/editar.php
@@ -1,5 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <link rel="icon" href="https://fezvrasta.github.io/bootstrap-material-design/favicon.ico">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/my.css" rel="stylesheet">
+  <link href="./Product example for Bootstrap_files/product.css" rel="stylesheet">
+  <title>Clinica Dolores</title>
+  <style type="text/css">
+    :root #content>#right>.dose>.dosesingle,
+    :root #content>#center>.dose>.dosesingle {
+      display: none !important;
+    }
+  </style>
+  
+  <!--Barra de navegacion, Inicio, Login, Servicios, Acerca de... -->
+</head>
+
 <?php 
     $cedprof = $_GET['cedprof'];
 
-    
+    require_once __DIR__.'..\database\Connection.php';
+    require_once __DIR__.'..\database\Funcion.php'; 
+    use PostgreSQLPHPconnect\Connection as Connection;
+    use PostgreSQLPHPconnect\Funcion as Funcion;
+    try{
+        // create a PostgreSQL database connection
+        $pdo = Connection::get()->connect("admin");
+        $funcion = new Funcion($pdo);
+        // get all stocks data
+        $result = $funcion->getMedicos();  
+
+    }catch (PDOException $e){
+        // report error message
+        echo $e->getMessage();
+    }
+
 ?>
+
+<script type="text/javascript" src="js/jquery-3.4.0.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <script>
+    Holder.addTheme('thumb', {
+      bg: '#55595c',
+      fg: '#eceeef',
+      text: 'Thumbnail'
+    });
+  </script>
+
+  <footer class="container py-5">
+    <div class="row">
+      <div class="col-12 col-md text-center">
+        <small class="d-block mb-3 text-muted">© 2019</small>
+      </div>
+    </div>
+    <!--Script alertas-->
+    
+  </footer>
+</html>
