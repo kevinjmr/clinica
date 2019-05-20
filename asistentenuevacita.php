b314799b4f2dc61618817b7c9bff5f06526cff34
sailar305 <sailar305@hotmail.com>
2019-05-20 16:00:11 -0500

    cuidado

---
+++ asistentenuevacita.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="https://fezvrasta.github.io/bootstrap-material-design/favicon.ico">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/my.css" rel="stylesheet">
+  <link href="./Product example for Bootstrap_files/product.css" rel="stylesheet">
+  <title>Clinica Dolores</title>
+  <style type="text/css">
+    :root #content>#right>.dose>.dosesingle,
+    :root #content>#center>.dose>.dosesingle {
+      display: none !important;
+    }
+  </style>
+</head>
+<body>
+	<nav class="light-blue site-header py-1">
+      <div class="col-12 container d-flex flex-column flex-md-row">
+        <div class="col-9 container d-flex flex-column flex-md-row">
+          <h2 class="py-2 px-2 d-none d-md-inline-block" href="#modal-select-paciente" data-toggle="modal" >Medico</h2>
+          <button type="button" class="btn btn-info" style="max-width: 40%; max-height:  50%;" data-dismiss="modal">info</button>
+        </div>
+        <div class="col-5 container d-flex flex-column flex-md-row">
+          <a class="text-dark py-2 px-2 d-none d-md-inline-block" href="#">Agenda</a>
+          <a class="text-dark py-2 px-2 d-none d-md-inline-block" href="#">Regresar</a>
+            <a class="text-dark py-2 px-2 d-none d-md-inline-block" href="#">Cerrar sesion</a>
+        </div>
+      </div>
+  </nav>
+<br>
+  <div class="col-12 container d-flex flex-column flex-md-row">
+  	<div class="col-2 container d-flex flex-column flex-md-row">
+  	</div>	
+  	<div class="col-8 container d-flex flex-column flex-md-row">
+  	<div class="col-2 container d-flex flex-column flex-md-row">
+  	</div>
+  	<div class="col-4 container d-flex flex-column flex-md-row">
+  			<input type="date" style="border-radius: 5px;" name="">
+  		</div>
+  		<div class="col-2 container d-flex flex-column flex-md-row">
+  	</div>
+  	</div>
+  	
+  	<div class="col-2 container d-flex flex-column flex-md-row">
+  	</div>
+  </div>
+
+  <div class="col-12 container d-flex flex-column flex-md-row">
+  	<div class="col-3 container d-flex flex-column flex-md-row">
+  	</div>
+  	<div class="col-6 container d-flex flex-column flex-md-row">
+  		 <table class="table table-sm table-bordered" style=" background-color: white " cellspacing="0">
+                <thead>
+                    <tr>
+                      <th>HORA:</th>
+                      <th>PACIENTE:</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">7:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">7:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">8:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">8:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">9:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">9:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">10:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">10:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">11:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">11:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">12:00 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">12:30 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">1:00 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">1:30 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">2:00 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">2:30 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+
+                </tbody>
+            </table>
+  	</div>
+  	<div class="col-3 container d-flex flex-column flex-md-row">
+  	</div>
+  </div>
+
+
+</body>
+</html>
