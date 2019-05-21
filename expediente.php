b92b4a6bc5189115546c8e32220b30d9a54fcaa2
sailar305 <sailar305@hotmail.com>
2019-05-20 19:28:47 -0500

    cambia men

--- expediente.php
+++ expediente.php
@@ -25,8 +25,6 @@
 	<nav class="light-blue site-header py-1">
       <div class="col-12 container d-flex flex-column flex-md-row">
         <div class="col-9 container d-flex flex-column flex-md-row">
-          <h2 class="py-2 px-2 d-none d-md-inline-block" href="#modal-select-paciente" data-toggle="modal" >Medico</h2>
-          <button type="button" class="btn btn-info" style="max-width: 40%; max-height:  50%;" data-dismiss="modal">info</button>
         </div>
         <div class="col-3 container d-flex flex-column flex-md-row">
           	<a class="text-dark py-2 px-2 d-none d-md-inline-block" href="medico.php">Agenda</a>
@@ -38,8 +36,12 @@
 <br>
   <div class="col-2 container d-flex flex-column flex-md-row"></div>
   <div class="col-8 containerflex-md-row">
+  	<div class="container d-flex flex-column flex-md-row">
+  		<h4 class="font-weight-bold px-2">Expediente: </h4>
+  		<h4>mario aguilar perez</h4>
+
+  	</div>
   	
-  	<h4 class="py-2">Expediente: mario aguilar perez</h4>
   	<h6>(seleciona para ver detalles)</h6>
             <table class="table table-sm table-bordered" style=" background-color: white " cellspacing="0">
                 <thead>
