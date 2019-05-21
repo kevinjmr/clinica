ae3227928e4c1b56ba55728252867b147898316e
sailar305 <sailar305@hotmail.com>
2019-05-20 20:26:35 -0500

    chingatewey

--- medico.php
+++ medico.php
@@ -52,89 +52,105 @@ if( $varcomparacion == null || $varcomparacion == ''){
 
   <div class="col-12 container d-flex flex-column flex-md-row">
     <div class="col-2 container d-flex flex-column flex-md-row">
+    </div>  
+    <div class="col-8 container d-flex flex-column flex-md-row">
+    <div class="col-2 container d-flex flex-column flex-md-row">
     </div>
-    <div class="col-8 container flex-md-row">
-    <div class="col-3 container flex-md-row"></div>
-    <div class="col-4 container flex-md-row">
-      <h3>Citas de la semana</h3>
+    <div class="col-4 container d-flex flex-column flex-md-row">
+        <input type="date" style="border-radius: 5px;" name="">
+      </div>
+      <div class="col-2 container d-flex flex-column flex-md-row">
     </div>
-    <div class="col-3 container flex-md-row"></div>
-        <table class="table table-sm table-bordered" style=" background-color: white " cellspacing="0">
+    </div>
+    
+    <div class="col-2 container d-flex flex-column flex-md-row">
+    </div>
+  </div>
+  <br>
+
+  <div class="col-12 container d-flex flex-column flex-md-row">
+    <div class="col-3 container d-flex flex-column flex-md-row">
+    </div>
+    <div class="col-6 container d-flex flex-column flex-md-row">
+       <table class="table table-sm table-bordered" style=" background-color: white " cellspacing="0">
                 <thead>
                     <tr>
-                      <th>Fecha</th>
-                      <th>Paciente</th>
-                      <th>Medico</th>
-                      <th>Descripcion</th>
+                      <th>HORA:</th>
+                      <th>PACIENTE:</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                      <td><a onclick="editarmedico('Work1')">1996/10/19 9:30</a></td>
+                      <td><a onclick="editarmedico('Work1')">7:00 A.M.</a></td>
                       <td>Mariana leal sandoval</td>
-                      <td>Cesar Reynel Ortiz</td>
-                      <td>♥</td>
                     </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">7:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">8:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">8:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">9:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">9:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">10:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">10:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">11:00 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">11:30 A.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">12:00 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">12:30 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">1:00 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">1:30 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">2:00 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+                    <tr>
+                      <td><a onclick="editarmedico('Work1')">2:30 P.M.</a></td>
+                      <td>Mariana leal sandoval</td>
+                    </tr>
+
                 </tbody>
             </table>
-    
     </div>
-    <div class="col-2 container d-flex flex-column flex-md-row">
-    </div>    
-    
-  </div>
-
-
-
-
-  <!-- modal para ver datos del Medico -->
-<div class="modal fade" id="modal-select-paciente">
-    <div class="modal-dialog modal-lg"  role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="myModalLabel">Datos del Medico:</h5>
-        </div>
-        <div class="modal-body">
-          <form id="c_form-h" class="">
-            <div class="form-group row">
-              <div class="col-6 d-flex flex-column">
-                <label>Ced_Prof:</label>
-                <label  type="text" class="form-control" name="ced_prof"><?php echo trim($curp); ?></label>
-                </div>
-                <div class="col-6 d-flex flex-column" >
-                <label>Nombre    :</label>
-                <label  type="text" class="form-control" name="Nombre"><?php echo $result[0]; echo $result[1]; echo $result[2];?></label>
-                </div>
-            </div>
-            <div class="form-group row">
-              <div class="col-6 d-flex flex-column">
-              <label>Telefono  :</label>
-                <label type="text" class="form-control" name="Telefono"><?php echo $result[3]; ?></label>
-                </div>
-                <div class="col-6 d-flex flex-column">
-                <label>Direccion :</label>
-                <label type="text" class="form-control" name="Direccion"><?php echo $result[4]; ?></label>
-                </div>
-            </div>
-            <div class="form-group row">
-              <div class="col-6 d-flex flex-column">
-              <label>Especialidad:</label>
-                <label type="text" class="form-control" name="Especialidad"><?php echo $result[5]; ?></label>
-                </div>
-                 <div class="col-6 d-flex flex-column">
-                 <label>Escuela:</label>
-                <label type="text" class="form-control" name="Escuela"><?php echo $result[6]; ?></label>
-                </div> 
-            </div>        
-          </form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-info" data-dismiss="modal">Cerrar</button>
-        </div>
-      </div>
+    <div class="col-3 container d-flex flex-column flex-md-row">
     </div>
   </div>
-
 
 
   </body>
